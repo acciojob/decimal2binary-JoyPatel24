9e15ae717fb574b692fd5ba9c045705796a51622
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2023-05-12 00:11:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function threeSum(arr, target) {
 // write your code here
-  
+const result = [];
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      if (sum === target) {
+        result.push([arr[i], arr[left], arr[right]]);
+        left++;
+        right--;
+      } else if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return result;  
 }
 
 module.exports = threeSum;
