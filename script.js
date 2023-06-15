140f950fb354b49098d2a29265790b5c9ef73ab5
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2023-06-15 09:16:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 function decimalToBinary(decimal) {
   // Handle special case for 0
   if (decimal === 0) {
-    return '0';
+    return '0'; // Special case for decimal number 0
   }
 
   let binary = '';
   while (decimal > 0) {
-    // Get the remainder when dividing by 2
-    const remainder = decimal % 2;
-    // Add the remainder to the binary string
-    binary = remainder + binary;
-    // Divide the decimal by 2
+    binary = (decimal % 2) + binary;
     decimal = Math.floor(decimal / 2);
   }
 
-  console.log(binary);
+  return binary;
 }
 
